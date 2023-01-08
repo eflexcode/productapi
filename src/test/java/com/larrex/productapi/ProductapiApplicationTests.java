1256905c9fbc0d8d8811c633d31888ac7e3d0865
Eze Larry Ifeanyi <ezelarry379@gmail.com>
2023-01-08 20:32:00 +0100

    added some test

--- src/test/java/com/larrex/productapi/ProductapiApplicationTests.java
+++ src/test/java/com/larrex/productapi/ProductapiApplicationTests.java
@@ -3,20 +3,52 @@ package com.larrex.productapi;
 import com.larrex.productapi.model.Product;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 import org.springframework.web.client.RestTemplate;
+
+import static org.junit.Assert.*;
 
 @SpringBootTest
 class ProductapiApplicationTests {
 
-	@Test
-	void testGetProduct() {
+    @Test
+    void testGetProduct() {
 
-		RestTemplate restTemplate = new RestTemplate();
+        RestTemplate restTemplate = new RestTemplate();
 
-		Product product = restTemplate.getForObject("http://localhost:8080/productapi/products/2", Product.class);
+        Product product = restTemplate.getForObject("http://localhost:8080/productapi/products/2", Product.class);
+
+        assertNotNull(product);
+        assertEquals("Hp laptop", product.getName());
+
+    }
+
+    @Test
+    void testCrateProduct() {
+
+        RestTemplate restTemplate = new RestTemplate();
+        Product product = new Product();
+        product.setName("Yandex car");
+        product.setDescription("A self driving car from yandex");
+        product.setPrice(5000);
+        Product newProduct = restTemplate.postForObject("http://localhost:8080/productapi/products/", product, Product.class);
+        assertNotNull(newProduct);
+        assertNotNull(newProduct.getId());
+        assertEquals("Yandex car",newProduct.getName());
+
+    }
 
 
+    @Test
+    void testUpdateProduct() {
 
-	}
+        RestTemplate restTemplate = new RestTemplate();
 
+        Product product = restTemplate.getForObject("http://localhost:8080/productapi/products/2", Product.class);
+        product.setPrice(2999);
+
+        restTemplate.put("http://localhost:8080/productapi/products/2",product);
+        
+
+    }
 }
